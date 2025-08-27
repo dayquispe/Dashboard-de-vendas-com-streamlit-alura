3ebde1bde32d28033be1546ba808921e34d8e77b
Dayana <dayana.tquispe@gmail.com>
2025-08-26 21:37:47 -0300

    Adicionando filtros para o DataFrame

--- pages/Dados brutos.py
+++ pages/Dados brutos.py
@@ -18,10 +18,40 @@ with st.expander("Colunas"):
 st.sidebar.title("Filtros") 
 with st.sidebar.expander("Nome do produto"):
     produtos = st.multiselect("Selecione os produtos", dados["Produto"].unique(), dados["Produto"].unique())
+with st.sidebar.expander("Categoria do produto"):
+    categoria = st.multiselect("Selecione as categorias do produto", dados["Categoria do Produto"].unique(), dados["Categoria do Produto"].unique())
 with st.sidebar.expander("Preço do produto"):
-    preco = st.slider("Selecione o preço", 0, 5000, (0, 5000))
+    preco = st.slider("Selecione o preço", 0, 5000, value=(0, 5000))
+with st.sidebar.expander("Frete da venda"):
+    frete = st.slider("Frete", 0, 5000, (0, 5000))
 with st.sidebar.expander("Data da compra"):
     data_compra = st.date_input("Selecione a data", (dados["Data da Compra"].min(), dados["Data da Compra"].max()))
+with st.sidebar.expander('Vendedor'):
+    vendedores = st.multiselect('Selecione os vendedores', dados['Vendedor'].unique(), dados['Vendedor'].unique())
+with st.sidebar.expander("Local da compra"):
+    local_compra = st.multiselect("Selecione o local da compra do produto", dados["Local da compra"].unique(), dados["Local da compra"].unique())
+with st.sidebar.expander('Avaliação da compra'):
+    avaliacao = st.slider('Selecione a avaliação da compra',1,5, value = (1,5))
+with st.sidebar.expander("Tipo de pagamento"):
+    tipo_pagamento = st.multiselect("Seleciona o tipo de pagamento", dados["Tipo de pagamento"].unique(), dados["Tipo de pagamento"].unique())
+with st.sidebar.expander('Quantidade de parcelas'):
+    qtd_parcelas = st.slider('Selecione a quantidade de parcelas', 1, 24, value=(1,24))
 
-st.dataframe(dados)
+query = '''
+Produto in @produtos and \
+`Categoria do Produto` in @categoria and \
+@preco[0] <= `Preço` <= @preco[1] and \
+@frete[0] <= `Frete` <= @frete[1] and \
+@data_compra[0] <= `Data da Compra` <= @data_compra[1] and \
+Vendedor in @vendedores and \
+`Local da compra` in @local_compra and \
+@avaliacao[0] <= `Avaliação da compra` <= @avaliacao[1] and \
+`Tipo de pagamento` in @tipo_pagamento and \
+@qtd_parcelas[0] <= `Quantidade de parcelas` <= @qtd_parcelas[1]
+'''
+dados_filtrados = dados.query(query)
+dados_filtrados = dados_filtrados[colunas]
 
+st.dataframe(dados_filtrados)
+
+st.markdown(f"A tabela possui :blue[{dados_filtrados.shape[0]}] linhas e :blue[{dados_filtrados.shape[1]}] colunas")
